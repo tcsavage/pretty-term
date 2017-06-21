356237e7e0bd288f2cae379005f6159ceaa92d3e
Tom Savage <tcsavage@gmail.com>
2017-06-21 14:23:28 +0100

    Prep for clojars

--- project.clj
+++ project.clj
@@ -1,2 +1,4 @@
-(defproject pretty-term "SNAPSHOT"
+(defproject tcsavage/pretty-term "0.0.1"
+  :description "Hiccup-like DSL for pretty-printed terminal output."
+  :url "https://github.com/tcsavage/pretty-term"
   :dependencies [[org.clojure/clojure "1.8.0"]])
